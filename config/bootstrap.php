98c044e0b898296f60013117e98a42c51b97e042
Takeshi Ueda <uedatakeshi@gmail.com>
2015-08-26 18:30:25 +0900

    Preparing for heroku4

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -187,7 +187,7 @@ Plugin::load('Migrations');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
-if (Configure::read('debug')) {
+if (Configure::read('debug') && !isset($_ENV['CAKE_ENV'])) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
 
